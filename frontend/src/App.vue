ddf7e35012bcd32ad28f86afb3b666ff4b67b4f1
Cameron Kilgore <ghostfreeman@gmail.com>
2023-11-12 15:03:48 -0600

    Added fetch, installed axios, and other stuff

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { ref } from 'vue'
+const messages = ref(null)
+fetch("http://localhost:8080/")
+    .then(response => response.json())
+    .then(data => announcements)
+
 import message from './components/galactic/message.vue'
 </script>
 
