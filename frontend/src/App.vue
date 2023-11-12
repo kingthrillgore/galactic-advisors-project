bd680d376cac74a42d4cc2894f871234073f1db0
Cameron Kilgore <ghostfreeman@gmail.com>
2023-11-12 16:30:01 -0600

    Development breakpoint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 let messages = ref(null)
-fetch("http://localhost:8080/")
+fetch("http://localhost:8080/", {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }})
     .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
     .then(data => messages = data)
+    .catch(error=> {
+        console.error("Guru Meditation:", error)
+    })
     .finally(console.log(messages))
 
 import message from './components/galactic/message.vue'
@@ -14,14 +24,12 @@ import message from './components/galactic/message.vue'
         Messages
     </header>
 
-    <main>
-        <message 
-            v-for="message in messages"
-        />
-    </main>
-
     <div v-if="messages">
         <p>Messages Found</p>
+
+        <div v-for="m in messages">
+            <p>{{ m }}</p>
+        </div>
     </div>
 
     <div v-if="!messages">
